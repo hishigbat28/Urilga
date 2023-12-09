e8f6dc37ef15f40913087a869b5090b379815181
Baigala <83354086+Baigala@users.noreply.github.com>
2023-12-09 18:47:51 +0800

    +

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,5 +38,9 @@ window.addEventListener("scroll", scrollHeader);
 let parallax = new Rellax(".parallax");
 
 /*=============== GSAP ANIMATION ===============*/
-
+gsap.from(".home__village", 1.2, { opacity: 0, y: 100, delay: 0.1 });
+gsap.from(".home__pine", 1.2, { opacity: 0, y: 150, delay: 0.3 });
+gsap.from(".home__mountain-2", 1.2, { opacity: 0, x: 150, delay: 0.5 });
+gsap.from(".home__mountain-3", 1.2, { opacity: 0, x: -150, delay: 0.6 });
+gsap.from(".home__mountain-1", 1.2, { opacity: 0, x: 250, delay: 0.7 });
 /*=============== SCROLL REVEAL ANIMATION ===============*/
